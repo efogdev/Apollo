f9b865b2f0e41f6908408f5bc773d3079f39102a
Orr Zohar <orrzohar@users.noreply.huggingface.co>
2024-12-02 23:46:16 +0000

    Update apollo/model/multimodal_encoder/siglip_encoder.py

--- apollo/model/multimodal_encoder/siglip_encoder.py
+++ apollo/model/multimodal_encoder/siglip_encoder.py
@@ -23,28 +23,3 @@ class SiglipVisionTower(VisionTower):
     def feature_select(self, image_forward_outs):
         image_features = image_forward_outs.hidden_states[self.select_layer]
         return image_features
-
-
-class SiglipVisionTowerS2(VisionTowerS2):
-    def __init__(self, model_name_or_path:str, config: PretrainedConfig, vision_config: PretrainedConfig):
-        super().__init__(model_name_or_path, config, vision_config)
-        if vision_config is not None:
-            self.vision_config = SiglipVisionConfig.from_pretrained(model_name_or_path)
-        else: 
-            self.vision_config = vision_config
-            
-        self.vision_tower_name = model_name_or_path
-        self.vision_processor = SiglipImageProcessor.from_pretrained(self.vision_tower_name)
-        self.vision_tower = SiglipVisionModel.from_pretrained(self.vision_tower_name)
-        
-        # Make sure it crops/resizes the image to the largest scale in self.scales to maintain high-res information
-        self.vision_processor.size['shortest_edge'] = self.scales[-1]
-        self.vision_processor.crop_size['height'] = self.vision_processor.crop_size['width'] = self.scales[-1]
-        
-        self.hidden_size = self.vision_config.hidden_size
-        self.W = self.H = self.vision_config.image_size//self.vision_config.patch_size
-        self.T = 1
-
-    def feature_select(self, image_forward_outs):
-        image_features = image_forward_outs.hidden_states[self.select_layer]
-        return image_features
