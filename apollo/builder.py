761c88b0288955ed28c749015782b57640226fc5
Orr Zohar <orrzohar@users.noreply.huggingface.co>
2024-12-02 23:00:50 +0000

    Update apollo/builder.py

--- apollo/builder.py
+++ apollo/builder.py
@@ -22,7 +22,6 @@ from transformers import (
     AutoTokenizer,
     AutoModelForCausalLM,
     AutoConfig,
-    BitsAndBytesConfig,
     PretrainedConfig,
     PreTrainedModel,
 )
@@ -48,14 +47,6 @@ def load_pretrained_model(
 
     if load_8bit:
         kwargs["load_in_8bit"] = True
-    elif load_4bit:
-        kwargs["load_in_4bit"] = True
-        kwargs["quantization_config"] = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_compute_dtype=torch.float16,
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_quant_type="nf4",
-        )
     else:
         kwargs["torch_dtype"] = torch.float16
 
